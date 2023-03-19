6e0898a4333f2143533b55a9714bc69b9e05a0f9
Eduardo Veintimilla <eduardojavy@gmail.com>
2023-03-19 18:56:43 -0500

    [UPD] StreamBuilder para carga de peliculas footer de manera infinita

--- movies_app/lib/src/providers/peliculas_provider.dart
+++ movies_app/lib/src/providers/peliculas_provider.dart
@@ -1,6 +1,8 @@
+
 import 'package:http/http.dart' as http;
 
 import 'dart:convert';
+import 'dart:async';
 
 import 'package:movies_app/src/models/pelicula_model.dart';
 
@@ -9,6 +11,21 @@ class PeliculasProvider {
   String _apikey = '0dd952d720ceabe567d57ab74faabf9a';
   String _url = 'api.themoviedb.org';
   String _language = 'es-ES';
+
+  int _popularesPage = 0;
+  bool _cargando = false;
+
+  List<Pelicula> _populares = [];
+  
+  final _popularesStreamController = StreamController<List<Pelicula>>.broadcast();
+
+  Function(List<Pelicula>) get popularesSink => _popularesStreamController.sink.add;
+
+  Stream<List<Pelicula>> get popularesStream => _popularesStreamController.stream;
+
+  void disposeStreams() {
+    _popularesStreamController.close(); 
+  }
 
   Future<List<Pelicula>> _procesarRespuesta(Uri url) async{
     final resp = await http.get(url);
@@ -29,14 +46,26 @@ class PeliculasProvider {
   }
 
   Future<List<Pelicula>> getPopulares() async{
+    
+    if(_cargando) return [];
+
+    _cargando = true;
+
+    _popularesPage++;
 
     final url = Uri.https(_url, '3/movie/popular', {
       'api_key': _apikey,
-      'language': _language
+      'language': _language,
+      'page': _popularesPage.toString()
     });
 
-    return await _procesarRespuesta(url);
+    final resp = await _procesarRespuesta(url);
 
+    _populares.addAll(resp);
+    popularesSink(_populares);
+    _cargando = false;
+
+    return resp;
   }
 
 }
--- movies_app/lib/src/widgets/movie_horizontal.dart
+++ movies_app/lib/src/widgets/movie_horizontal.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:movies_app/src/models/pelicula_model.dart';
 
@@ -5,25 +7,65 @@ class MovieHorizontal extends StatelessWidget {
   // const MovieHorizontal({super.key});
 
   final List<Pelicula> peliculas;
+  final Function siguientePagina;
 
-  MovieHorizontal({required this.peliculas});
+  MovieHorizontal({required this.peliculas, required this.siguientePagina});
+
+  final _pageController = PageController(
+    initialPage: 1,
+    viewportFraction: 0.3
+  );
 
   @override
   Widget build(BuildContext context) {
 
     final _screenSize = MediaQuery.of(context).size;
 
+    _pageController.addListener( () {
+        if(_pageController.position.pixels >= _pageController.position.maxScrollExtent - 200){
+          siguientePagina();
+        }
+      }
+    );
+
     return Container(
       height: _screenSize.height * 0.2,
-      child: PageView(
+      child: PageView.builder(
         pageSnapping: false,
-        controller: PageController(
-          initialPage: 1,
-          viewportFraction: 0.3
-        ),
-        children: _tarjetas(context),
+        controller: _pageController,
+        itemCount: peliculas.length,
+        itemBuilder: (context, i) => _tarjeta(context, peliculas[i]),
+        // children: _tarjetas(context),
       ),
     );
+  }
+
+  Widget _tarjeta(BuildContext context, Pelicula pelicula){
+    return Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(right: 15.0),
+            child: Column(
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10.0),
+                  child: FadeInImage(
+                    image: NetworkImage(pelicula.getPosterImg()),
+                    placeholder: AssetImage('assets/img/no-image.jpg'),
+                    fit: BoxFit.cover,
+                    height: 110.0,
+                  ),
+                ),
+                Text(
+                  pelicula.title.toString(),
+                  overflow: TextOverflow.ellipsis,
+                  style: Theme.of(context).textTheme.caption,
+                )
+              ],
+            ),
+          ),
+        ],
+      );
   }
 
   List<Widget> _tarjetas(BuildContext context) {
